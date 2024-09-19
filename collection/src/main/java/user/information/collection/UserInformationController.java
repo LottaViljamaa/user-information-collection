e33ac29dd12c1b1839a99aceaf812a2c9f373051
LottaViljamaa <lotta.viljamaa@tuni.fi>
2024-09-19 15:27:54 +0300

    Lisätty endpoint uuden käyttäjän lisäämiselle.

--- collection/src/main/java/user/information/collection/UserInformationController.java
+++ collection/src/main/java/user/information/collection/UserInformationController.java
@@ -1,5 +1,6 @@
 package user.information.collection;
 
+import org.springframework.http.HttpStatus;
 import user.information.collection.userInformation.UserInformationCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +14,17 @@ public class UserInformationController {
     @Autowired
     private UserInformationService userInformationService;
 
+    //Kaikkien käyttäjien tietojen hakeminen
     @GetMapping("/all")
     public List<UserInformationCollection> findUsers() {
         return userInformationService.getAllUsers();
+    }
+
+    //Uuden käyttäjän lisäys
+    @PutMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addUser(@RequestBody UserInformationCollection userDetails) {
+        userInformationService.addUser(userDetails);
+        return "User added successfully.";
     }
 }
